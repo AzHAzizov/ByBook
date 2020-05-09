3b924c2357d3cec18af070383099404476ee321e
Aziz <azjhazizov>
2020-05-10 01:00:38 +0500

    Created :  new layout , nav and constructerd routs

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,13 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::get('/about', 'PagesController@about');
+Route::get('/contact', 'PagesController@contact');
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
